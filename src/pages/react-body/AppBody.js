3b282894f68fcc31bfc85e0aecd1ef914c1d7231
CryingEvil <erwinpurnama567gc@gmail.com>
2022-05-29 00:01:07 +0700

    nambah css

--- src/pages/react-body/AppBody.js
+++ src/pages/react-body/AppBody.js
@@ -20,6 +20,35 @@ class AppBody extends React.Component {
     componentDidMount() {
         this.getItem();
     }
+
+    fillAuthors(authors){
+        const names = "";
+        authors.map((val,indx)=>{
+            names = names+ ", "+ val.value;
+        })
+
+        console.log(names);
+        //return names;
+    }
+
+    fillStateItem(){
+        const temp = [];
+        this.state.data.map((val, index) => {
+            temp.push({
+                title: val.volumeInfo.title,
+                publisher: val.volumeInfo.publisher,
+                rating: val.volumeInfo.averageRating ? val.volumeInfo.averageRating : 0,
+                img: val.volumeInfo.imageLinks.thumbnail,
+                bookId: val.id,
+                author: val.volumeInfo.authors? val.volumeInfo.authors :["-"]
+            })
+        })
+        this.setState({
+            books: temp
+        })
+        console.log(temp);
+    }
+
     getItem() {
         axios.get("https://www.googleapis.com/books/v1/volumes?q=%7Bkeyword")
             .then(res => {
@@ -28,18 +57,7 @@ class AppBody extends React.Component {
                     error: false,
                     data: res.data.items
                 });
-                this.state.data.map((val, i) => {
-                    this.state.dataFilter.push({
-                        title: val.volumeInfo.title,
-                        publisher: val.volumeInfo.publisher,
-                        rating: val.volumeInfo.averageRating ? val.volumeInfo.averageRating : 0,
-                        img: val.volumeInfo.imageLinks.thumbnail,
-                        bookId: val.id
-
-                    });
-                    this.state.books = this.state.dataFilter;
-                });
-                console.log(this.state.dataFilter);
+                this.fillStateItem();
             })
             .catch(error => {
                 this.setState({
@@ -54,25 +72,13 @@ class AppBody extends React.Component {
             dataFilter: [],
             books: []
         })
-        const temp = [];
         axios.get("https://www.googleapis.com/books/v1/volumes?q=" + event)
             .then(res => {
                 this.setState({
                     error: false,
                     data: res.data.items,
                 }, () => {
-                    this.state.data.map((val, index) => {
-                        temp.push({
-                            title: val.volumeInfo.title,
-                            publisher: val.volumeInfo.publisher,
-                            rating: val.volumeInfo.averageRating ? val.volumeInfo.averageRating : 0,
-                            img: val.volumeInfo.imageLinks.thumbnail,
-                            bookId: val.id
-                        })
-                    })
-                    this.setState({
-                        books: temp
-                    })
+                    this.fillStateItem();
                 });
             })
             .catch(error => {
@@ -97,9 +103,10 @@ class AppBody extends React.Component {
         })
     }
 
-    on
+    
     render() {
         const { data, error, errMsg, dataFilter, books, searchVal } = this.state;
+        var tempName = "";
         return (
             <div className="main-body">
                 <div className="row">
@@ -108,7 +115,6 @@ class AppBody extends React.Component {
                             <input className="search-txt" id="search-text"
                                 onKeyDown={(ev) => {
                                     if (ev.key === 'Enter') {
-                                        console.log(searchVal)
                                         this.clickSearch(searchVal)
                                     }
                                 }}
@@ -134,6 +140,19 @@ class AppBody extends React.Component {
                                                     <td className="col-6">{book.title}</td>
                                                 </tr>
                                                 <tr>
+                                                    <td className="col-1">Authors</td>
+                                                    <td className="col-6">{book.author.map((name,i)=>{
+                                                        if(i ==0)
+                                                        {
+                                                            tempName = name
+                                                        }else{
+                                                            tempName = tempName+', '+name
+                                                        }
+                                                        
+                                                        
+                                                    })}<label>{tempName}</label></td>
+                                                </tr>
+                                                <tr>
                                                     <td className="col-1">Publisher</td>
                                                     <td className="col-6">{book.publisher ? book.publisher : '-'}</td>
                                                 </tr>
@@ -144,7 +163,7 @@ class AppBody extends React.Component {
                                             </tbody>
                                         </table>
                                     </td>
-                                    <td><button className="save" onClick={() => { this.clickSave(book) }}>Favorite</button></td>
+                                    <td><button className="fav-btn" onClick={() => { this.clickSave(book) }}><i className="fas fa-heart"></i></button></td>
 
                                 </tr>
                             )}
